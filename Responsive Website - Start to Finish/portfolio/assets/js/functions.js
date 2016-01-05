48bb3a09fcdd0549938e5c4e2e2c8eac175f6635
pacificano <tris.pacificano@gmail.com>
2016-01-05 11:37:20 +1100

    Responsive Website

--- Responsive Website - Start to Finish/portfolio/assets/js/functions.js
+++ Responsive Website - Start to Finish/portfolio/assets/js/functions.js
@@ -1,5 +1,20 @@
 $( document ).ready(function() {
 
-  // Get started!
+	smoothScroll(300);
 
 });
+
+function smoothScroll (duration) {
+	$('a[href^="#"').on('click', function (event) {
+		
+		var target = $( $(this).attr('href') );
+
+		if ( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration);
+		}
+
+	});
+} 
