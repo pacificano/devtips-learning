0e0acfde3030e92f002a54d8de760b96f0bdce5a
pacificano <tris.pacificano@gmail.com>
2016-01-06 00:24:21 +1100

    added bouncejs animations. made them into

--- Responsive Website - Start to Finish/portfolio/assets/js/functions.js
+++ Responsive Website - Start to Finish/portfolio/assets/js/functions.js
@@ -1,9 +1,3 @@
-$( document ).ready(function() {
-
-	smoothScroll(300);
-
-});
-
 function smoothScroll (duration) {
 	$('a[href^="#"').on('click', function (event) {
 		
@@ -18,3 +12,51 @@ function smoothScroll (duration) {
 
 	});
 } 
+
+
+
+function workBelt () {
+	
+	$('.thumb-unit').click(function() {
+		
+		$('.work-belt').css('left', '-100%');
+
+	});
+
+	$('.work-box').click(function() {
+		
+		$('.work-belt').css('left', '0%');
+	});
+
+}
+
+
+
+function workLoad () {
+	
+	$.ajaxSetup( { cache: false } );
+
+	$('.thumb-unit').click(function() {
+
+		var $this = $(this),
+			newTitle = $this.find('strong').text(),
+			newFolder = $this.data('folder'),
+			spinner = '<div class="loader">Loading...</div>',
+			newHTML = '/work/' + newFolder + '.html';
+		
+		$('.project-load').html(spinner).load(newHTML);
+		$('.project-title').text(newTitle);
+
+	});
+
+}
+
+
+
+$( document ).ready(function() {
+
+	smoothScroll(300);
+	workBelt();
+	workLoad();
+
+});
